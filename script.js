a1b6b546b94ad8d8469ea8b940fc480faeeb2c2c
loriki7 <kikpubgm@gmail.com>
2022-11-27 17:58:20 +0200

    Final Touches

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ let weather = {
             "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
-        document.querySelector(".humidity").innerText = "Humidity " + humidity + " %";
-        document.querySelector(".wind").innerText = "Wind speed " + speed + " km/h";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + " %";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
         document.querySelector(".weather").classList.remove("loading");
 
         let imgName = name;
